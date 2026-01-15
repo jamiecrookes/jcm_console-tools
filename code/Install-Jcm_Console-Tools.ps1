6bf1f9f881757716fe1bcc1ef0fc4f7db38397f0
Jamie Crookes <jamie@dznt.net>
2026-01-15 22:39:09 +0000

    Installation script now supports multi-os types. Basic Who functionallity now built for the Get-WhoLogged function.

--- code/Install-Jcm_Console-Tools.ps1
+++ code/Install-Jcm_Console-Tools.ps1
@@ -1,14 +1,41 @@
-## Create the destination directory if it doesn't exist
-if (!(Test-Path "$HOME\Documents\PowerShell\Modules\Jcm_Console-Tools")) {
-    Write-Host "Creating Destination Directory"
-    
-    New-Item -ItemType Directory -Path $HOME\Documents\PowerShell\Modules\Jcm_Console-Tools | Out-Null
+# Windows Installation Procedure
+if ($isWindows) {
+    ## Create the destination directory if it doesn't exist
+    if (!(Test-Path "$HOME\Documents\PowerShell\Modules\Jcm_Console-Tools")) {
+        Write-Host "Creating Destination Directory"
+        New-Item -ItemType Directory -Path $HOME\Documents\PowerShell\Modules\Jcm_Console-Tools | Out-Null
+    }
+    ## Copy module files
+    try {
+        Write-Host "Copying Module Files"
+        Copy-Item Jcm_Console-Tools.psm1 $HOME\Documents\PowerShell\Modules\Jcm_Console-Tools -ErrorAction Stop
+    } catch {
+        Write-Host "Error Copying Module Files $_.ExceptionMessage"
+    }
 }
-## Copy module files
-try {
-    Write-Host "Copying Module Files"
 
-    Copy-Item Jcm_Console-Tools.psm1 $HOME\Documents\PowerShell\Modules\Jcm_Console-Tools -ErrorAction Stop
-} catch {
-    Write-Host "Error Copying Module Files $_.ExceptionMessage"
+# Linux Installation Procedure
+if ($isLinux) {
+    ## Create the destination directory if it doesn't exist
+    if (!(Test-Path "$home/.local/share/powershell/Modules/Jcm_Console-Tools")) {
+        Write-Host "Creating Destination Directory"
+        New-Item -ItemType Directory -Path "$home/.local/share/powershell/Modules/Jcm_Console-Tools" | Out-Null
+    }
+}
+
+# macOS Installation Procedure
+if ($isMacos) {
+    Write-Host "macOS Operating System Detected"
+    ## Create the destination directory if it doesn't exist
+    if (!(Test-Path "$home/.local/share/powershell/Modules/Jcm_Console-Tools")) {
+        Write-Host "..Creating Destination Directory"
+        New-Item -ItemType Directory -Path "$home/.local/share/powershell/Modules/Jcm_Console-Tools" | Out-Null
+    }
+    ## Copy module files
+    try {
+        Write-Host "..Copying Module Files"
+        Copy-Item Jcm_Console-Tools.psm1 "$home/.local/share/powershell/Modules/Jcm_Console-Tools" -ErrorAction Stop
+    } catch {
+        Write-Host "..Error Copying Module Files $_.ExceptionMessage"
+    }
 }
